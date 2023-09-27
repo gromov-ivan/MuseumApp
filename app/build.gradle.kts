ddec35351304eb074c9106176272065415c207ce
Ainara <larranagaainara@gmail.com>
2023-09-27 23:23:31 +0300

    added coil dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ android {
 }
 
 dependencies {
+
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
     val retrofit_version = "2.9.0"
     implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
     implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
