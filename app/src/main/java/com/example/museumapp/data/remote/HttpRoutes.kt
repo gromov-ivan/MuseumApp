c76ead9058592e4a8b9dc5538a76725a1bb3528a
Ainara <larranagaainara@gmail.com>
2023-09-29 14:19:56 +0300

    wip: setting proper URLs

--- app/src/main/java/com/example/museumapp/data/remote/HttpRoutes.kt
+++ app/src/main/java/com/example/museumapp/data/remote/HttpRoutes.kt
@@ -2,7 +2,7 @@ package com.example.museumapp.data.remote
 
 object HttpRoutes {
 
-    private const val BASE_URL = "https://users.metropolia.fi/~ainaral/json"
+    private const val BASE_URL = "https://users.metropolia.fi/~ainaral"
     const val MUSEUM_ENDPOINT = "$BASE_URL/TuusulaMuseum.json"
 
 }
