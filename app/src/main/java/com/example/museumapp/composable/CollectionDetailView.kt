74fad8a4baab05503df9e2f0946c6342b8b8c2a5
Ainara <larranagaainara@gmail.com>
2023-10-02 11:18:48 +0300

    CollectionDetailView UI done.

--- app/src/main/java/com/example/museumapp/composable/CollectionDetailView.kt
+++ app/src/main/java/com/example/museumapp/composable/CollectionDetailView.kt
@@ -1,36 +1,68 @@
 package com.example.museumapp.composable
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.example.museumapp.data.remote.dto.MuseumItem
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CollectionDetailView(selectedItem: MuseumItem) {
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        modifier = Modifier.fillMaxSize()
+        .verticalScroll(rememberScrollState())
     ) {
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Collections detail View")
-            Text(text = "Title: ${selectedItem.title}")
+            val painter =
+                rememberAsyncImagePainter(
+                    ImageRequest.Builder(LocalContext.current)
+                        .data(data = selectedItem.images).apply(block = fun ImageRequest.Builder.() {
+                            // You can configure image loading options here if needed
+                        }).build()
+                )
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f)
+                    .scale(0.8f),
+                contentScale = ContentScale.Crop,
+                alignment = Alignment.TopStart,
+            )
+            Text(text = "${selectedItem.title}, ${selectedItem.year}.",
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .padding(start = 30.dp, top = 2.dp, end = 20.dp, bottom = 10.dp))
+            Text(text = "Artist: ${selectedItem.nonPresenterAuthorsName}",
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .padding(start = 30.dp, top = 2.dp, end = 30.dp, bottom = 10.dp))
+            Text(text = "${selectedItem.imageDescription}",
+                modifier = Modifier
+                    .padding(start = 30.dp, top = 10.dp, end = 30.dp, bottom = 30.dp))
         }
     }
 }
