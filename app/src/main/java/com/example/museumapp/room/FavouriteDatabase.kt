2353a1c3d0adf54ecd1d6364685edd3eb75b8047
Ainara <larranagaainara@gmail.com>
2023-10-07 20:16:19 +0300

    modified database

--- app/src/main/java/com/example/museumapp/room/FavouriteDatabase.kt
+++ app/src/main/java/com/example/museumapp/room/FavouriteDatabase.kt
@@ -5,23 +5,25 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [FavouriteItem::class], version = 1)
+@Database(entities = [FavouriteItem::class], version = 1, exportSchema = false)
 abstract class FavouriteDatabase : RoomDatabase() {
-    abstract fun favouriteItemDao(): FavouriteItemDao
+    abstract val favouriteDao: FavouriteDao
 
     companion object {
         @Volatile
         private var INSTANCE: FavouriteDatabase? = null
-
         fun getInstance(context: Context): FavouriteDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    FavouriteDatabase::class.java,
-                    "favourite_database"
-                ).build()
-                INSTANCE = instance
-                instance
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context,
+                        FavouriteDatabase::class.java, "favourite_database"
+                    )
+                        .fallbackToDestructiveMigration().build()
+                    INSTANCE = instance
+                }
+                return instance
             }
         }
     }
