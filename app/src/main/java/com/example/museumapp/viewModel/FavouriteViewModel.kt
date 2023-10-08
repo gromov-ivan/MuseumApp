e515b71d88fd6bf6ca38b8e60250e279b6b339bd
Ainara <larranagaainara@gmail.com>
2023-10-08 14:10:26 +0300

    added saving and deleting function

--- app/src/main/java/com/example/museumapp/viewModel/FavouriteViewModel.kt
+++ app/src/main/java/com/example/museumapp/viewModel/FavouriteViewModel.kt
@@ -3,11 +3,28 @@ package com.example.museumapp.viewModel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import com.example.museumapp.room.FavouriteDatabase
 import com.example.museumapp.room.FavouriteItem
+import kotlinx.coroutines.launch
 
 class FavouriteViewModel(application: Application): AndroidViewModel(application) {
     private val db = FavouriteDatabase.getInstance(application)
+    private val favouriteDao = db.favouriteDao
+
+    // Function to insert a new favorite item
+    fun saveFavoriteItem(favouriteItem: FavouriteItem) {
+        viewModelScope.launch {
+            favouriteDao.insertFavouriteItem(favouriteItem)
+        }
+    }
+
+    // Function to delete a favorite item
+    fun deleteFavoriteItem(favouriteItem: FavouriteItem) {
+        viewModelScope.launch {
+            favouriteDao.deleteFavouriteItem(favouriteItem)
+        }
+    }
 
     fun getAllFavourites(): LiveData<List<FavouriteItem>> {
         return db.favouriteDao.getAllFavouriteItemsLiveData()
