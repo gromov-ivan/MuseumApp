1f8c983afb874e53f0ea3ce5ea3829824e48a58c
Ainara <larranagaainara@gmail.com>
2023-10-08 22:13:34 +0300

    added isFavourite function

--- app/src/main/java/com/example/museumapp/viewModel/FavouriteViewModel.kt
+++ app/src/main/java/com/example/museumapp/viewModel/FavouriteViewModel.kt
@@ -3,6 +3,7 @@ package com.example.museumapp.viewModel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.map
 import androidx.lifecycle.viewModelScope
 import com.example.museumapp.room.FavouriteDatabase
 import com.example.museumapp.room.FavouriteItem
@@ -11,10 +12,18 @@ import kotlinx.coroutines.launch
 class FavouriteViewModel(application: Application): AndroidViewModel(application) {
     private val db = FavouriteDatabase.getInstance(application)
     private val favouriteDao = db.favouriteDao
+    val favouriteItems = favouriteDao.getAllFavouriteItemsLiveData()
+
+    // Function to check if an item is a favorite by ID
+    fun isFavourite(itemId: String): Boolean {
+        val items = favouriteItems.value
+        return items?.any { it.id == itemId } == true
+    }
 
     // Function to insert a new favorite item
     fun saveFavoriteItem(favouriteItem: FavouriteItem) {
         viewModelScope.launch {
+            favouriteItem.isFavorite = true
             favouriteDao.insertFavouriteItem(favouriteItem)
         }
     }
