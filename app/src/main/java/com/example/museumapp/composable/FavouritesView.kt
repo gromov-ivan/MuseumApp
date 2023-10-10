0cea00e6e168f15595dd2553eaa62ebaf64bdea9
sallyzy0123 <sally_zy@protonmail.com>
2023-10-10 14:56:56 +0300

    add the navigation to the right bottom icon

--- app/src/main/java/com/example/museumapp/composable/FavouritesView.kt
+++ app/src/main/java/com/example/museumapp/composable/FavouritesView.kt
@@ -1,32 +1,32 @@
 package com.example.museumapp.composable
 
-import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.museumapp.viewModel.FavouriteViewModel
+import androidx.compose.material.*
+import androidx.compose.material.icons.filled.Animation
+import androidx.compose.runtime.*
+import androidx.compose.material3.Icon
+
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun FavouritesView(favouriteViewModel: FavouriteViewModel){
+fun FavouritesView(favouriteViewModel: FavouriteViewModel) {
     val navController = rememberNavController()
 
     // Observe the LiveData
@@ -34,18 +34,7 @@ fun FavouritesView(favouriteViewModel: FavouriteViewModel){
 
     NavHost(navController, startDestination = "Home") {
         composable("Home") {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(text = "Favourite page")
-                Button(onClick = {
-                    navController.navigate("FavouriteAnimatedView")
-                }) {
-                    Text(text = "FavouriteAnimatedView")
-                }
-                // Display favorite items as cards
+            Box {
                 LazyColumn(
                     modifier = Modifier.fillMaxSize(),
                     contentPadding = PaddingValues(16.dp)
@@ -56,6 +45,17 @@ fun FavouritesView(favouriteViewModel: FavouriteViewModel){
                             favouriteViewModel = favouriteViewModel
                         )
                     }
+                }
+                // Add FloatingActionButton for navigation
+                FloatingActionButton(
+                    onClick = {
+                        navController.navigate("FavouriteAnimatedView")
+                    },
+                    modifier = Modifier
+                        .padding(16.dp) // Add appropriate padding
+                        .align(Alignment.BottomEnd)
+                ) {
+                    Icon(imageVector = Icons.Default.Animation, contentDescription = "Navigate")
                 }
             }
         }
